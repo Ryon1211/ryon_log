3d6d0f0dc885485c8c119c372383c350f735a9ba
Ryon1211 <ryon1211sco@gmail.com>
2021-03-16 11:08:41 +0900

    fix パンくずリストの文字サイズ修正

--- components/Breadcrumb.vue
+++ components/Breadcrumb.vue
@@ -1,14 +1,14 @@
 <template>  
     <div class="container">
-      <nav class="breadcrumb px-6 py-2" aria-label="breadcrumbs">
+      <nav class="breadcrumb breadcrumb-fontsize px-6 py-2" aria-label="breadcrumbs">
         <ul>
           <li><nuxt-link class="" to="/">Home</nuxt-link></li>
           <li v-for="location in locations" :key="location.name">
             <template v-if="location.path">
-              <nuxt-link :to="location.path">{{ location.name }}</nuxt-link>
+              <nuxt-link :to="location.path" class="text-hidden-overflow">{{ location.name }}</nuxt-link>
             </template>
-            <template v-else>
-              <span class="px-3">{{ location.name }}</span>
+            <template v-if="!location.path">
+              <span class="px-3 text-hidden-overflow">{{ location.name }}</span>
             </template>
           </li>
         </ul>
@@ -41,21 +41,25 @@ export default {
         this.locations.push(
           { name: "Profile", path: "" });
       }else if(routeName === "blog-page-page") {
-        this.locations.push(
-          { name: "Blog", path: "/blog/page/1" },
-          { name: `Page ${ this.props }`, path: '' });
+          this.locations.push(
+            { name: "Blog", path: "/blog/page/1" },
+            { name: `Page ${ this.props }`, path: '' });
+
       }else if(routeName === "blog-slug") {
-        this.locations.push(
-          { name: "Blog", path: "/blog/page/1" },
-          { name: this.props.fields.title, path: '' });
+          this.locations.push(
+            { name: "Blog", path: "/blog/page/1" },
+            { name: this.props.fields.title, path: '' });
+
       }else if(routeName === "blog-tags-slug-page") {
-        this.locations.push(
-          { name: "Blog", path: "/blog/page/1" },
-          { name: this.props.fields.name, path: '' });
+          this.locations.push(
+            { name: "Blog", path: "/blog/page/1" },
+            { name: this.props.fields.name, path: '' });
+
       }else if(routeName === "blog-search-page") {
-        this.locations.push(
-          { name: "Blog", path: "/blog/page/1" },
-          { name: `「${ this.props }」の検索結果`, path: '' });
+          this.locations.push(
+            { name: "Blog", path: "/blog/page/1" },
+            { name: `「${ this.props }」の検索結果`, path: '' });
+            
       }else if(routeName === "works-slug") {
         this.locations.push(
           { name: "Profile", path: "/profile" },
